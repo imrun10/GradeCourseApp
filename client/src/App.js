789a9adc491218d8312a84d200485be5dfde5d04
imrun10 <nasir.imrun10@gmail.com>
2023-03-09 17:41:26 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,88 @@
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [file, setFile] = useState();
+  const [array, setArray] = useState([]);
 
+  const fileReader = new FileReader();
 
+  const handleOnChange = (e) => {
+    setFile(e.target.files[0]);
+  };
 
-  function recieveCsv(){
-    console.log('recieved CSV')
-  }
+  const csvFileToArray = string => {
+    const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
+    const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
 
+    const array = csvRows.map(i => {
+      const values = i.split(",");
+      const obj = csvHeader.reduce((object, header, index) => {
+        object[header] = values[index];
+        return object;
+      }, {});
+      return obj;
+    });
+
+    setArray(array);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (file) {
+      fileReader.onload = function (event) {
+        const text = event.target.result;
+        csvFileToArray(text);
+      };
+
+      fileReader.readAsText(file);
+    }
+  };
+
+  const headerKeys = Object.keys(Object.assign({}, ...array));
 
   return (
-    <div className="App">
+    <div style={{ textAlign: "center" }}>
+      <h1>REACTJS CSV IMPORT EXAMPLE </h1>
+      <form>
+        <input
+          type={"file"}
+          id={"csvFileInput"}
+          accept={".csv"}
+          onChange={handleOnChange}
+        />
 
-          <form>
-                <input type={"file"} accept={".csv"} />
-                <button onClick={recieveCsv}>IMPORT CSV</button>
-          </form>
-     
+        <button
+          onClick={(e) => {
+            handleOnSubmit(e);
+          }}
+        >
+          IMPORT CSV
+        </button>
+      </form>
 
+      <br />
 
+      <table>
+        <thead>
+          <tr key={"header"}>
+            {headerKeys.map((key) => (
+              <th>{key}</th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {array.map((item) => (
+            <tr key={item.id}>
+              {Object.values(item).map((val) => (
+                <td>{val}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
