102592d1dc1e8135b52b6ef8d1c90500f3b8f85e
imrun10 <nasir.imrun10@gmail.com>
2023-03-12 21:15:06 +0300

    seconf

---
+++ client/src/App.js
@@ -0,0 +1,101 @@
+import './App.css';
+import React, { useState } from "react";
+import Axios from 'axios';
+
+function App() {
+  const [file, setFile] = useState();
+  const [array, setArray] = useState([]);
+
+  const sendFile = () => {
+    console.log("send")
+    Axios.post("http://localhost:3001/update",
+    {arr: array}).then((console.log("Sent")))
+
+  };
+
+  const fileReader = new FileReader();
+
+  const handleOnChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const csvFileToArray = string => {
+    const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
+    const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
+
+    const array = csvRows.map(i => {
+      const values = i.split(",");
+      const obj = csvHeader.reduce((object, header, index) => {
+        object[header] = values[index];
+        return object;
+      }, {});
+      return obj;
+    });
+
+    setArray(array);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (file) {
+      fileReader.onload = function (event) {
+        const text = event.target.result;
+        csvFileToArray(text);
+      };
+
+      fileReader.readAsText(file);
+    }
+  };
+
+  const headerKeys = Object.keys(Object.assign({}, ...array));
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>REACTJS CSV IMPORT EXAMPLE </h1>
+      <form>
+        <input
+          type={"file"}
+          id={"csvFileInput"}
+          accept={".csv"}
+          onChange={handleOnChange}
+        />
+
+        <button
+          onClick={(e) => {
+            handleOnSubmit(e);
+            
+          }}
+        >
+          IMPORT CSV1234
+
+        </button>
+      </form>
+      <button onClick={()=>{sendFile();}}>send</button>
+ 
+      <br />
+
+      <table>
+        <thead>
+          <tr key={"header"}>
+            {headerKeys.map((key) => (
+              <th>{key}</th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {array.map((item) => (
+            <tr key={item.id}>
+              {Object.values(item).map((val) => (
+                <td>{val}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 const express =  require('express');
 const app = express();
 const mysql = require('mysql');
+const cors =require('cors');
+
+app.use(cors());
+
+app.use(express.json())
+
 
 
 app.get('/', (req, res) =>{
@@ -8,6 +14,12 @@ app.get('/', (req, res) =>{
     res.send("Hello world");
 
 })
+
+app.get('/update',(req, res) =>{
+    res.send(req.body.arr);
+    console.log("hello")
+})
+
 
 
 
