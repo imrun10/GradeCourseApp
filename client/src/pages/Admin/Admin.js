769708eb7d85a141cfb80a9a044e927d398a9aec
imrun10 <nasir.imrun10@gmail.com>
2023-05-07 08:50:03 +0300

    fix kind off

--- client/src/pages/Admin/Admin.js
+++ client/src/pages/Admin/Admin.js
@@ -1,37 +1,8 @@
 import { Admin } from "react-admin";
 import "../../App.css";
 
-import Courses from "../../components/Courses";
-import Navbar from "../../components/NavBar/NavBar";
 
 
-function Adm(){
-
-    const cards = [{CourseName: "Manage", CourseCode: "Accounts", FacultyName: "",code:3},{CourseName: "Manage", CourseCode: "Courses", FacultyName: "",code:3},{CourseName: "Manage", CourseCode: "Students", FacultyName: "",code:3},{CourseName: "Manage", CourseCode: "Instructors", FacultyName: "",code:3},]
-
-    
-    
-    
-    return (
-        <div style={{ textAlign: "center" }}>
-          <header>
-          <Navbar />
-          </header>
-    
-          <br />
-          <body>
-            <div class="container">
-              {/*this is how we loop through the importedCourses array and display the courses in the array*/}
-              {cards.map((e) => (
-                <div class="item">
-                  <Courses name={e.CourseName} id={e.CourseCode} proff={e.FacultyName} code={e.code} />
-                  {/*this is how we send data to the courses component, we send the course name, id and proff*/}
-                </div>
-              ))}
-            </div>
-          </body>
-        </div>
-      );
-    }
+function Adm(){return( <Admin />);}
 
 export default Adm;
