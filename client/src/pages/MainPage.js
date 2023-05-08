fab09172c655e410592b15719c6e3b7592cb6044
imrun10 <nasir.imrun10@gmail.com>
2023-05-08 16:03:56 +0300

    f

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -4,9 +4,30 @@ import Courses from "../components/Courses"; // this is how we import components
 import "bootstrap/dist/css/bootstrap.min.css"; // this is how we import bootstrap css
 import "../App.css"; // this is how we import css from other files
 import Navbar from "../components/NavBar/NavBar"; // this is how we import css from other files
+import { useCookies } from "react-cookie"; // this is how we import cookies from other files
+import { useEffect } from "react"; // this is how we import useEffect from other files
+
+import { Link } from "react-router-dom"; // this is how we import Link from other files
+
 
 function MainPage() {
 
+  //checks if security level is 3 and if it is displays a button  tthat redirects to the admin page
+  const [cookies, setCookie, removeCookie] = useCookies(['cookie-name']);
+  var securityCookie = cookies.security;
+  var emailCookie = cookies.email;
+    //checks if security level is 3 and if it is displays a button  tthat redirects to the admin page
+    if(securityCookie == 3){
+      function ButtonToAdmin(){
+        return <Link to="/admin" class="btn btn-primary">Admin Page</Link>
+      }}
+    else{
+      function ButtonToAdmin(){
+        return <div></div>
+      }
+    }
+
+  
   const cards = [{CourseName: "View", CourseCode: "Student", FacultyName: "Detailes",code:1},{CourseName: "View", CourseCode: "Course", FacultyName: "Detailes",code:1},]
   return (
     <div style={{ textAlign: "center" }}>
@@ -26,6 +47,10 @@ function MainPage() {
           ))}
         </div>
       </body>
+      <footer>
+
+
+      </footer>
     </div>
   );
 }
